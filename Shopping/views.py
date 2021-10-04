247fc908baa061bb29506a3b01d10ae35bd5c37e
davido2003 <70234277+davido2003@users.noreply.github.com>
2021-10-04 23:59:53 +0100

    Update views.py

--- Shopping/views.py
+++ Shopping/views.py
@@ -6,39 +6,30 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login ,logout
-from django.contrib.auth.decorators import login_required
+
 def web1(request):
     return render (request, 'index.html')
 def web2(request):
     Holds = Household.objects.all()
     return render (request, 'product2.html', {'Holds': Holds})
-@login_required(login_url='login')
 def web3(request):
     return render (request, 'single2.html')
-@login_required(login_url='login')
 def web4(request):
     return render (request, 'checkout.html')
-@login_required(login_url='login')
 def web5(request):
     return render(request, 'help.html')
 def web6(request):
     return render(request, 'payment.html')
-@login_required(login_url='login')
 def web7(request):
     Kitchs = Kitchen.objects.all()
     return render(request, 'product.html',{'Kitchs': Kitchs})
-@login_required(login_url='login')
 def web8(request):
     return render(request, 'single.html')
-@login_required(login_url='login')
 def web9(request):
     return render(request, 'terms.html')
-@login_required(login_url='login')
 def web10(request):
     return render(request, 'contact.html')
-@login_required(login_url='login')
-def web11(request):
-   
+def web11(request):   
         form = CreateUserForm()
         if request.method == 'POST':
             form = CreateUserForm(request.POST)
@@ -50,8 +41,6 @@ def web11(request):
         context = {'form':form}
         return render(request, 'registration.html', context)
 def loginpage(request):
-    
-    
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
